2c2f99454a3cbf39870c150e8f8cf5430a67007b
Masinde Odera <masindeodera@gmail.com>
2024-07-21 17:07:55 +0200

    updated phone number validation

--- src/views/ContactEditor.vue
+++ src/views/ContactEditor.vue
@@ -33,8 +33,14 @@
         <FormKit class="half" type="email" label="Email" v-model="user.email" />
         <FormKit
           class="half"
-          type="text"
+          type="tel"
           label="Phone number"
+          placeholder="xxx-xxx-xxxx"
+          validation="matches:/^[0-9]{3}-[0-9]{3}-[0-9]{4}$/"
+          :validation-messages="{
+            matches: 'Phone number must be in the format xxx-xxx-xxxx',
+          }"
+          validation-visibility="dirty"
           v-model="user.phoneNumber"
         />
         <FormKit
